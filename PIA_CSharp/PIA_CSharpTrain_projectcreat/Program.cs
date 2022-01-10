2093619dfb2851efc0682461e9081527223e79ff
wy.lee <wy.lee@gs.cwnu.ac.kr>
2022-01-10 14:12:20 +0900

    CSharp 1주차 review test

---
+++ PIA_CSharp/PIA_CSharpTrain_projectcreat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIA_CSharpTrain_projectcreat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sum={0}", total(20, 10, 40, 4, 7, 6, 44, 55, 2));
+            Console.WriteLine("sum={0}", total(30, 4, 5));
+        }
+        static int total(params int[] list)
+        {
+            int sum = 0;
+            for (int i = 0; i < list.Length; i++)
+                sum += list[i];
+            return sum;
+        }
+    }
+}
